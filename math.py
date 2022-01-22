957db8c55c31ae58ac9d3c618618ba4e3dbf87c1
Vaishnavi <viya1306@gmail.com>
2022-01-22 16:02:24 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+#Add Implementation
 diff add(x,y);
 pass
 
+#Subtract Implementatipn
 diff subtract(x,y);
 pass
 
+#Multiply Implementtion
 diff multiply(x,y);
 pass
 
+#Divide Implementation
 diff divide(x,y);
 pass
